1f0e727ceb0d91092364bfbd1db6f9105fd8a07b
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-23 17:00:18 +0200

    Update classme.py

--- classme.py
+++ classme.py
@@ -1,19 +1,17 @@
 import streamlit as st
-import pandas as pd
 from streamlit_tree_select import tree_select
 
 datapath1 = "./treelist"
 
-tree1 = pd.load_data(datapath1)
+with open(datapath1) as f:
+    contents = f.readlines()
 
 with st.sidebar:
     st.title("Streamlit-tree-select")
     st.subheader("A simple and elegant checkbox tree for Streamlit.")
-    
-    
 
     # Create nodes to display
-    nodes = tree1
+    nodes = contents
 
     return_select = tree_select(nodes)
     st.write(return_select)
